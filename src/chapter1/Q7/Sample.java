49c893dd91dabf7afbe256ff06d51998a512c469
Harutaka Suzuki <nodame0910g@gmail.com>
2025-04-16 00:35:17 +0900

    study202504160034

---
+++ src/chapter1/Q7/Sample.java
@@ -0,0 +1,10 @@
+package chapter1.Q7;
+
+public class Sample {
+    public static void main(String[] args) {
+        args = new String[] { "a", "b", "c" };
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}

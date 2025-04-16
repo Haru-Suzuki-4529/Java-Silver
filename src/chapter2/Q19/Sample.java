3cc7062779dc3433241d2a28ad0b6e1401a32d6d
Harutaka Suzuki <nodame0910g@gmail.com>
2025-04-16 23:26:48 +0900

    study2025004162320

---
+++ src/chapter2/Q19/Sample.java
@@ -0,0 +1,9 @@
+package chapter2.Q19;
+
+public class Sample {
+    public static void main(String[] args) {
+        String str = null;
+        str += "null";
+        System.out.println(str);
+    }
+}
---
+++ src/chapter2/Q22/Sample.java
@@ -0,0 +1,8 @@
+package chapter2.Q22;
+
+public class Sample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("abcde");
+        System.out.println(sb.capacity());
+    }
+}

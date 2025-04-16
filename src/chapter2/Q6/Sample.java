e8f7f1ccf58ecb4796d893a11dd09d8066e8f3fe
Harutaka Suzuki <nodame0910g@gmail.com>
2025-04-16 09:00:52 +0900

    study202504160900

---
+++ src/chapter2/Q6/Sample.java
@@ -0,0 +1,22 @@
+package chapter2.Q6;
+
+import java.util.ArrayList;
+
+public class Sample {
+    public static void main(String[] args) {
+
+        // varについて
+        // varを使った変数宣言
+        var a = 6;
+
+        // ラムダ式からは型を推論できない(コンパイルエラー)
+        // var b = () -> {}
+
+        // 配列の初期化子は型宣言から型を推論しているので{}のみで型推論できない(コンパイルエラー)
+        // var c = {1,2,3}
+
+        // <>ジェネリクスは何もしてされない場合は<Object>となる。
+        var d = new ArrayList<>();
+        System.out.println(d.getClass().getSimpleName());
+    }
+}

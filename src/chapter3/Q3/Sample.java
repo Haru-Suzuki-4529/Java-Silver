12b684ae7e8d83d307c52a6f56e9fdf9f0d48064
Harutaka Suzuki <nodame0910g@gmail.com>
2025-04-17 09:00:26 +0900

    study2025004170900

---
+++ src/chapter3/Q3/Sample.java
@@ -0,0 +1,12 @@
+package chapter3.Q3;
+
+public class Sample {
+    public static void main(String[] args) {
+        byte a = 127;
+        // byte b = 128; はコンパイルエラー
+        // byte b = (byte) 128はコンパイルと実行はできるがオーバーフロー;
+        int b = 10;
+        System.out.println(b++ + ++b * ++b);
+        System.out.println(10 + 12 * 13);
+    }
+}
